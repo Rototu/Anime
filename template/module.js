1ed0ddb332f346e411add937fc6bb7816601d710
Emanuel Farauanu <rototuro@gmail.com>
2016-06-03 16:31:04 +0300

    Adding Page

---
+++ template/module.js
@@ -0,0 +1,19 @@
+var MainModule = (function () {
+
+   return {
+
+      init: function () {
+
+      },
+
+      bindHandlers: function () {
+
+      }
+
+   };
+})();
+
+$(document).on("ready", function () {
+   MenuModule.init();
+   MenuModule.bindHandlers();
+});
