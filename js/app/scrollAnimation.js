fa0c5f525d9f6ee68cf2a16d0468f9f500caea6c
Emanuel Farauanu <rototuro@gmail.com>
2016-06-06 13:09:04 +0300

    Update scrollAnimation.js

--- js/app/scrollAnimation.js
+++ js/app/scrollAnimation.js
@@ -88,7 +88,7 @@ var ScrollModule = (function() {
             i = Math.floor( relativeTopPos / windowHeight );
             
             if (relativeTopPos >= windowHeight) {
-               $("#menu").addClass("affix");
+               $("#menu").addClass("affix").css("top", 0);
                console.log("menu fixed");
             }
             else {
@@ -124,7 +124,7 @@ var ScrollModule = (function() {
                "end": "window",
                "fn": {
                   "top": {
-                     "start": 120,
+                     "start": 110,
                      "end": 100,
                      "unit": "vh"
                   }
