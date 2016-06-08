e60678c199b7206dd8cafe246ab14f6e4c211a43
Emanuel Farauanu <rototuro@gmail.com>
2016-06-08 09:48:38 +0300

    Update scrollAnimation.js

--- js/app/scrollAnimation.js
+++ js/app/scrollAnimation.js
@@ -87,16 +87,16 @@ var ScrollModule = (function() {
 
          //buttonScroll function
          $("#scrollUp").click(function() {
-            console.log(i);
-            var mySect = Math.floor(relativeTopPos / windowHeight); 
+            var mySect = Math.floor(relativeTopPos / windowHeight);
+            console.log(mySect); 
             var scrollValue = (mySect - 1) * windowHeight - $("#menu").height();
             if( mySect == 2) scrollValue = $(window).height();
             else if( mySect == 1) scrollValue = 0;
             $("html, body").stop().animate({scrollTop: scrollValue}, {queue: false, duration: 1000, delay: 0, easing: "easeInOutCubic"});
          });
          $("#scrollDown").click(function() {
-            console.log(i);
             var mySect = Math.floor(relativeTopPos / windowHeight); 
+            console.log(mySect); 
             var scrollValue = (mySect + 1) * windowHeight - $("#menu").height();
             if( mySect == 0) scrollValue = $(window).height();
             $("html, body").stop().animate({scrollTop: scrollValue}, {queue: false, duration: 1000, delay: 0, easing: "easeInOutCubic"});
