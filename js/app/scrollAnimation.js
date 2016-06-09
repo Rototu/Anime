b1d638b8553221b63fc5c7331a607717b3ab7ca1
Emanuel Farauanu <rototuro@gmail.com>
2016-06-09 10:12:37 +0300

    Update scrollAnimation.js

--- js/app/scrollAnimation.js
+++ js/app/scrollAnimation.js
@@ -93,18 +93,18 @@ var ScrollModule = (function() {
 
          //buttonScroll function
          $("#scrollUp").click(function() {
-            var mySect = Math.floor( ( relativeTopPos + 10 ) / windowHeight );
+            var mySect = Math.floor((relativeTopPos + 10) / windowHeight);
             console.log(mySect); 
             var scrollValue = (mySect - 1) * windowHeight - $("#menu").height();
-            if( mySect == 2 ) scrollValue = $(window).height();
-            else if( mySect == 1 ) scrollValue = 0;
+            if((relativeTopPos + 10) / windowHeight >= 1.1 && (relativeTopPos + 10) / windowHeight < 3) scrollValue = $(window).height();
+            else if(mySect == 1) scrollValue = 0;
             $("html, body").stop().animate({scrollTop: scrollValue}, {queue: false, duration: 1000, delay: 0, easing: "easeInOutCubic"});
          });
          $("#scrollDown").click(function() {
-            var mySect = Math.floor( ( relativeTopPos + $("#menu").height() ) / windowHeight ); 
+            var mySect = Math.floor( (relativeTopPos + $("#menu").height()) / windowHeight ); 
             console.log(mySect); 
             var scrollValue = (mySect + 1) * windowHeight - $("#menu").height();
-            if( mySect == 0 ) scrollValue = $(window).height();
+            if(mySect == 0) scrollValue = $(window).height();
             $("html, body").stop().animate({scrollTop: scrollValue}, {queue: false, duration: 1000, delay: 0, easing: "easeInOutCubic"});
          });
 
