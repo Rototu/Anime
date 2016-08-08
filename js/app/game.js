0c1b91375eee422482a8948726d7c8c491925280
Emanuel Farauanu <rototuro@gmail.com>
2016-08-08 17:58:19 +0300

    message

--- js/app/game.js
+++ js/app/game.js
@@ -1,42 +1,72 @@
 var GameModule = (function() {
 
+   // global vars
    var controlsEnabled = false;
    var $mainChar = $("#mainChar");
    var playerMoving;
    var solids = [];
+   var gameOffset = {};
+
+   // char control vars
+   var playerMoveOptions = { queue: true, duration: 10, delay: 0, easing: "linear" },
+   moveValue, movementOrientation, currentKey;
+   var mainCharSpriteAnimating = false;
+   var listeningToKeyPress = true;
 
    return {
 
       init: function() {
+
+         // enable char controls after page is loaded
          controlsEnabled = true;
+
+         // get gameOffset and store it in global var
+         gameOffset.x = $("#gameBox").offset().left;
+         gameOffset.y = $("#gameBox").offset().top;
+
       },
 
       bindHandlers: function() {
+
+         $(window).on("resize", function() {
+            gameOffset.x = $("#gameBox").offset().left;
+            gameOffset.y = $("#gameBox").offset().top;
+         });
 
       },
 
       animateSprite: function($el, spriteSize, numberOfFrames, refreshRate) {
 
-         // animation vars
+         // frame id from sprite
          var currentFrame = 0;
 
-         // animate frame function
-         var spriteAnimate = function(){
+         // frameSelector
+         var frameSelect = function() {
             if(currentFrame < numberOfFrames) {
                currentFrame++;
             }
             if(currentFrame == numberOfFrames) {
                currentFrame = 0;
             }
+         };
+
+         // animate frame function
+         var animateSprite = function(){
+            frameSelect();
             $el.css("background-position", (-currentFrame)*spriteSize + "px " + (-$el.spriteLine)*spriteSize + "px" );
          };
 
+         // animation function
+         var startSpriteAnimation = function() {
+            $el.stop();
+            animateSprite();
+            $el.objectAnimation = setInterval(function() {
+               animateSprite();
+            }, refreshRate);
+         }
+
          // start animation
-         $el.stop();
-         spriteAnimate();
-         $el.objectAnimation = setInterval(function() {
-            spriteAnimate();
-         }, refreshRate);
+         startSpriteAnimation();
 
          // THESE ARE THE DIRECT EXTERNAL CONTROLS FOR THE SPRITE ANIMATION
          // $el.spriteLine = 0;
@@ -45,12 +75,16 @@ var GameModule = (function() {
       },
 
       disableSprite: function($el) {
+
+         // clear sprite animation interval
          clearInterval($el.objectAnimation);
+
       },
 
-      changeSpriteOrientation: function($el, orientation) {
+      changeSpriteOrientation: function($el, movementOrientation) {
 
-         switch (orientation) {
+         // transform movementOrientation string to line id from sprite
+         switch (movementOrientation) {
 
             case "up":
             $el.spriteLine = 3;
@@ -74,10 +108,10 @@ var GameModule = (function() {
 
       },
 
-      randomSprite: function($el, size, optionNumber) {
+      randomSprite: function($el, size, numberOfOptions) {
 
          // get random option
-         var randomOption = Math.floor(Math.random() * (optionNumber));
+         var randomOption = Math.floor(Math.random() * (numberOfOptions));
 
          // set sprite based on random option
          $el.css("background-position", (-randomOption)*size + "px 0" );
@@ -98,34 +132,35 @@ var GameModule = (function() {
             };
          });
 
-         console.log(solids);
-
-         // periodical collision test
+         // collision test set as global interval
          window.detectCollision = setInterval(function() {
 
             // loop through solid objects
             for(var obj1 in solids) {
 
-               // update position and vars
+               // update position and id for current solid
+               var currentId = solids[obj1].item.prop('id');
                solids[obj1].x = solids[obj1].item.position().left;
                solids[obj1].y = solids[obj1].item.position().top;
-               var currentId = solids[obj1].item.prop('id');
 
-               //test collision
+               // loop through rest of solids
                for(var obj2 in solids) {
+
+                  // if not same solids test for collision
                   if(obj1 != obj2) {
 
                      // Collision conditions
-                     var bool1 = (solids[obj1].x < solids[obj2].x + solids[obj2].width);
-                     var bool2 = (solids[obj1].x + solids[obj1].width > solids[obj2].x);
-                     var bool3 = (solids[obj1].y < solids[obj2].y + solids[obj2].height);
-                     var bool4 = (solids[obj1].height + solids[obj1].y > solids[obj2].y);
+                     var test1 = (solids[obj1].x < solids[obj2].x + solids[obj2].width);
+                     var test2 = (solids[obj1].x + solids[obj1].width > solids[obj2].x);
+                     var test3 = (solids[obj1].y < solids[obj2].y + solids[obj2].height);
+                     var test4 = (solids[obj1].height + solids[obj1].y > solids[obj2].y);
 
-                     // verify conditions
-                     if (bool1 && bool2 && bool3 && bool4) {
+                     // test and update
+                     if (test1 && test2 && test3 && test4) {
                         solids[obj1].colliding = true;
+                     } else {
+                        solids[obj1].colliding = false;
                      }
-                     else solids[obj1].colliding = false;
 
                   }
                }
@@ -136,47 +171,43 @@ var GameModule = (function() {
 
       mainCharControls: function() {
 
-         // char control vars
-         var moveOptions = { queue: true, duration: 1, delay: 0, easing: "linear" };
-         var moveDirection;
-         var orientation;
-         var moveStarted = false;
-         var listeningToKeyPress = true;
+         // keypress handler
+         $(document).keydown(function(e) {
 
-         // key control
-         if(listeningToKeyPress) {
+            // proceed if listening to keyboard is enabled
+            if(listeningToKeyPress && controlsEnabled) {
 
-            $(document).keydown(function(e) {
-
+               // test for arrow keys
                if(e.which == 37 || e.which == 38 || e.which == 39 || e.which == 40) {
                   playerMoving = true;
                   listeningToKeyPress = false;
+                  currentKey = e.which;
                }
 
                switch(e.which) {
 
                   // left key
                   case 37:
-                  moveDirection = {left: "-=2px"};
-                  orientation = "left";
+                  moveValue = {left: "-=2px"};
+                  movementOrientation = "left";
                   break;
 
                   // up key
                   case 38:
-                  moveDirection = {top: "-=2px"};
-                  orientation = "up";
+                  moveValue = {top: "-=2px"};
+                  movementOrientation = "up";
                   break;
 
                   // right key
                   case 39:
-                  moveDirection = {left: "+=2px"};
-                  orientation = "right";
+                  moveValue = {left: "+=2px"};
+                  movementOrientation = "right";
                   break;
 
                   // down key
                   case 40:
-                  moveDirection = {top: "+=2px"};
-                  orientation = "down";
+                  moveValue = {top: "+=2px"};
+                  movementOrientation = "down";
                   break;
 
                   // exit for other keys
@@ -184,94 +215,141 @@ var GameModule = (function() {
 
                }
 
-            });
+               // switch sprite orientation;
+               GameModule.changeSpriteOrientation($mainChar, movementOrientation);
 
-         }
+            }
+         });
 
-
-         // if keyup stop action
+         // on arrow keyup stop action
          $(document).keyup(function(e) {
-            if(e.which == 37 || e.which == 38 || e.which == 39 || e.which == 40) {
+
+            // test if released key coincides with the first pressed key
+            if(currentKey == e.which && controlsEnabled) {
                playerMoving = false;
                $mainChar.clearQueue();
                listeningToKeyPress = true;
             }
-         });
 
+         });
+      },
+
+      mainCharAnimation: function() {
+
+         // collision vars
          var collided = false;
          var badOrientation;
+         var posX;
+         var insideBox = true;
+         var escaping = false;
 
          // character control refresh function (listen ==> action)
          var charMovement = setInterval(function() {
 
-            if(playerMoving && controlsEnabled) {
-               if(solids[0].colliding) {
+            // get player coordinates
+            posX = $mainChar.offset().left;
+            posY = $mainChar.offset().top;
 
+            // test if player is inside of game borders
+            if(posX <= gameOffset.x + 5 || posX >= gameOffset.x - 37 + 800 || posY <= gameOffset.y + 10 || posY >= gameOffset.y - 47 + 450) {
+               insideBox = false;
+            } else insideBox = true;
+
+            // test if character should be moving
+            if(playerMoving && controlsEnabled) {
+
+               // test if mainChar is colliding with another solid
+               if(solids[0].colliding || !insideBox && !escaping) {
+
+                  
+
+                  // stop movement and declare collision locally
                   if(!collided) {
                      collided = true;
-                     badOrientation = orientation;
+                     badOrientation = movementOrientation;
                      $mainChar.clearQueue();
-                     moveDirection = null;
+                     moveValue = null;
                      $mainChar.css({
                         left: $mainChar.x + "px",
                         top: $mainChar.y + "px"
                      });
                   }
 
+                  // force escape direction on player
                   else if(collided) {
 
-                     var bool1 = (badOrientation == "left" && orientation == "right");
-                     var bool2 = (orientation == "left" && badOrientation == "right");
-                     var bool3 = (badOrientation == "up" && orientation == "down");
-                     var bool4 = (orientation == "up" && badOrientation == "down");
+                     // set escape direction conditions
+                     var bool1 = (badOrientation == "left" && movementOrientation == "right");
+                     var bool2 = (badOrientation == "right" && movementOrientation == "left");
+                     var bool3 = (badOrientation == "up" && movementOrientation == "down");
+                     var bool4 = (badOrientation == "down" && movementOrientation == "up");
 
-                     if(bool1 || bool2 || bool3 || bool4) {
+                     // test for escape conditions
+                     if(badOrientation != movementOrientation) {
+
+                        // declare escape from collision
+                        badOrientation = null;
+                        collided = false;
+                        escaping = true;
                         setTimeout(function() {
-                           badOrientation = null;
-                           collided = false;
-                        }, 200);
+                           escaping = false;
+                        }, 1000);
+
                      }
+
+                     // if not escaping then set current pos to last know right pos
                      else {
                         $mainChar.clearQueue();
-                        moveDirection = null;
+                        moveValue = null;
                         $mainChar.css({
                            left: $mainChar.x + "px",
                            top: $mainChar.y + "px"
                         });
                      }
+
                   }
 
                }
 
-               if(!solids[0].colliding) {
-                  $mainChar.x = $mainChar.position().left;
-                  $mainChar.y = $mainChar.position().y;
+               // test if sprite animation cycle has started
+               if(!mainCharSpriteAnimating) {
+
+                  // start sprite animation cycle
+                  GameModule.animateSprite($mainChar, 32, 3, 100);
+                  mainCharSpriteAnimating = true;
+
                }
 
-               if(!moveStarted) {
-                  GameModule.changeSpriteOrientation($mainChar, orientation);
-                  GameModule.animateSprite($mainChar, 32, 3, 200);
-                  moveStarted = true;
-               }
-               $mainChar.animate(moveDirection, moveOptions);
+               // move character in received direction with set options
+               $mainChar.animate(moveValue, playerMoveOptions);
+
             }
-            else {
-               if(moveStarted) {
-                  $mainChar.stop();
-                  GameModule.disableSprite($mainChar);
-                  moveStarted = false;
-               }
+
+            // on keyUp if player is moving stop all its animation cycles
+            else if(mainCharSpriteAnimating) {
+               $mainChar.stop();
+               GameModule.disableSprite($mainChar);
+               mainCharSpriteAnimating = false;
             }
+
          }, 20);
 
-      }
+         // if player is not colliding set current pos as a good pos
+         setInterval(function() {
+            if(!solids[0].colliding && insideBox) {
+               $mainChar.x = $mainChar.position().left;
+               $mainChar.y = $mainChar.position().y;
+            }
+         }, 100);
 
+      }
    };
 })();
 
 $(document).on("ready", function() {
    GameModule.init();
    GameModule.bindHandlers();
-   GameModule.mainCharControls();
    GameModule.solidCollision();
+   GameModule.mainCharControls();
+   GameModule.mainCharAnimation();
 });
